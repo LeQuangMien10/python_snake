93694701eea0b4ccce669a3475aa3a3f87c87af2
LeQuangMien10 <lequangmien10@gmail.com>
2025-02-25 20:51:00 +0700

    Added basic logic

---
+++ main.py
@@ -0,0 +1,72 @@
+import pygame
+from config import *
+import snake
+
+def snake_control():
+    if event.key == pygame.K_UP or event.key == pygame.K_w:
+        if snake.velocity == RIGHT_VELOCITY or snake.velocity == LEFT_VELOCITY:
+            snake.velocity = UP_VELOCITY
+    elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
+        if snake.velocity == RIGHT_VELOCITY or snake.velocity == LEFT_VELOCITY:
+            snake.velocity = DOWN_VELOCITY
+    elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
+        if snake.velocity == UP_VELOCITY or snake.velocity == DOWN_VELOCITY:
+            snake.velocity = LEFT_VELOCITY
+    elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+        if snake.velocity == UP_VELOCITY or snake.velocity == DOWN_VELOCITY:
+            snake.velocity = RIGHT_VELOCITY
+
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Snake Game")
+
+snake = snake.Snake(velocity=LEFT_VELOCITY)
+clock = pygame.time.Clock()
+
+paused = False
+
+running = True
+
+
+def display_pause_screen():
+    snake.draw(screen)
+    font = pygame.font.Font(None, 100)
+    text = font.render("PAUSED", True, WHITE)
+    text_rect = text.get_rect()
+    screen.blit(text, ((SCREEN_WIDTH - text_rect.width) // 2, (SCREEN_HEIGHT - text_rect.height) // 2))
+
+
+def handle_event():
+    global running, paused
+    if event.type == pygame.QUIT:
+        running = False
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_ESCAPE:
+            paused = not paused
+        if not paused:
+            snake_control()
+
+
+while running:
+    # Check for event
+    for event in pygame.event.get():
+        handle_event()
+
+    # Draw screen
+    screen.fill(BACKGROUND_COLOR)
+    pygame.draw.line(screen, WHITE, LINE_START, LINE_END, LINE_WIDTH)
+
+    # Draw snake
+    if not paused:
+        snake.move()
+        snake.draw(screen)
+    else:
+        display_pause_screen()
+
+    #Update screen
+    pygame.display.flip()
+
+    clock.tick(10) # 10 FPS
+
+pygame.quit()
---
+++ snake.py
@@ -0,0 +1,34 @@
+import pygame
+from config import *
+
+class Snake:
+    def __init__(self, velocity, start_x=600, start_y = 400, length=3):
+        self.body = [pygame.Rect(start_x + i * BODY_SIZE, start_y, BODY_SIZE, BODY_SIZE)
+                     for i in range(length)]
+        self.colors = [HEAD_COLOR] + [BODY_COLOR] * (length - 2) + [TAIL_COLOR]
+        self.velocity = velocity
+
+    def draw(self, screen):
+        for rect, color in zip(self.body, self.colors):
+            pygame.draw.rect(screen, color, rect)
+
+    def move(self):
+        head_x, head_y = self.body[0].topleft
+        new_x = head_x + self.velocity[0]
+        new_y = head_y + self.velocity[1]
+
+        if new_y < LINE_START[1]:
+            new_y = SCREEN_HEIGHT - BODY_SIZE
+        elif new_y >= SCREEN_HEIGHT:
+            new_y = LINE_START[1]
+
+        if new_x < 0:
+            new_x = SCREEN_WIDTH - BODY_SIZE
+        elif new_x >= SCREEN_WIDTH:
+            new_x = 0
+        new_head = pygame.Rect(new_x, new_y, BODY_SIZE, BODY_SIZE)
+
+
+        self.body.insert(0, new_head)
+        self.body.pop()
+        # print(self.body)
