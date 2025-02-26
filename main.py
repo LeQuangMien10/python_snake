0346badb1d999ffc78d05dbde15d0786c161d92e
LeQuangMien10 <lequangmien10@gmail.com>
2025-02-26 13:41:44 +0700

    Added game over logic

--- main.py
+++ main.py
@@ -3,51 +3,108 @@ from config import *
 import snake
 import food
 
+
 def snake_control():
     if event.key == pygame.K_UP or event.key == pygame.K_w:
-        if snake.velocity == RIGHT_VELOCITY or snake.velocity == LEFT_VELOCITY:
-            snake.velocity = UP_VELOCITY
+        if snake_instance.velocity == RIGHT_VELOCITY or snake_instance.velocity == LEFT_VELOCITY:
+            snake_instance.velocity = UP_VELOCITY
     elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
-        if snake.velocity == RIGHT_VELOCITY or snake.velocity == LEFT_VELOCITY:
-            snake.velocity = DOWN_VELOCITY
+        if snake_instance.velocity == RIGHT_VELOCITY or snake_instance.velocity == LEFT_VELOCITY:
+            snake_instance.velocity = DOWN_VELOCITY
     elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
-        if snake.velocity == UP_VELOCITY or snake.velocity == DOWN_VELOCITY:
-            snake.velocity = LEFT_VELOCITY
+        if snake_instance.velocity == UP_VELOCITY or snake_instance.velocity == DOWN_VELOCITY:
+            snake_instance.velocity = LEFT_VELOCITY
     elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-        if snake.velocity == UP_VELOCITY or snake.velocity == DOWN_VELOCITY:
-            snake.velocity = RIGHT_VELOCITY
+        if snake_instance.velocity == UP_VELOCITY or snake_instance.velocity == DOWN_VELOCITY:
+            snake_instance.velocity = RIGHT_VELOCITY
+
 
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Snake Game")
 
-snake = snake.Snake(velocity=LEFT_VELOCITY)
-food = food.Food(snake.body)
+snake_instance = snake.Snake(velocity=LEFT_VELOCITY)
+food_instance = food.Food(snake_instance.body)
 clock = pygame.time.Clock()
 
 paused = False
-
+game_over = False
 running = True
 
 
+def reset_game():
+    global snake_instance, food_instance, game_over
+    snake_instance = snake.Snake(velocity=LEFT_VELOCITY)
+    food_instance = food.Food(snake_instance.body)
+    game_over = False
+
+
 def display_pause_screen():
-    snake.draw(screen)
+    snake_instance.draw(screen)
+    food_instance.draw(screen)
     font = pygame.font.Font(None, 100)
     text = font.render("PAUSED", True, WHITE)
     text_rect = text.get_rect()
-    screen.blit(text, ((SCREEN_WIDTH - text_rect.width) // 2, (SCREEN_HEIGHT - text_rect.height) // 2))
+    screen.blit(text, ((SCREEN_WIDTH - text_rect.width) // 2,
+                       (SCREEN_HEIGHT - text_rect.height) // 2))
 
+
+def display_game_over_screen():
+    snake_instance.draw(screen)
+    food_instance.draw(screen)
+    game_over_font = pygame.font.Font(None, 100)
+    game_over_text = game_over_font.render("GAME OVER", True, RED)
+    game_over_text_rect = game_over_text.get_rect()
+
+    guide_font = pygame.font.Font(None, 50)
+    guide_text = guide_font.render("Press R to restart!!", True, WHITE)
+    guide_text_rect = guide_text.get_rect()
+
+    screen.blit(game_over_text, ((SCREEN_WIDTH - game_over_text_rect.width) // 2,
+                                 (SCREEN_HEIGHT - (game_over_text_rect.height + guide_text_rect.height)) // 2))
+    screen.blit(guide_text, ((SCREEN_WIDTH - guide_text_rect.width) // 2,
+                             (SCREEN_HEIGHT - (game_over_text_rect.height + guide_text_rect.height)) // 2 +
+                             game_over_text_rect.height))
 
 def handle_event():
-    global running, paused
+    global running, paused, game_over
     if event.type == pygame.QUIT:
         running = False
     if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_ESCAPE:
+        if event.key == pygame.K_ESCAPE and not game_over:
             paused = not paused
+        elif event.key == pygame.K_r:
+            reset_game()
         if not paused:
             snake_control()
+
+
+def draw_screen():
+    screen.fill(BACKGROUND_COLOR)
+    pygame.draw.line(screen, WHITE, LINE_START, LINE_END, LINE_WIDTH)
+
+
+def display_game():
+    if game_over:
+        display_game_over_screen()
+    elif paused:
+        display_pause_screen()
+    else:
+        if snake_instance.body[0].topleft == food_instance.rect.topleft:
+            snake_instance.grow_snake()
+            food_instance.position = food_instance.reset_position(snake_instance.body)
+        snake_instance.move()
+        snake_instance.draw(screen)
+        food_instance.draw(screen)
+
+
+def check_for_game_over():
+    global game_over
+    if not game_over:
+        if snake_instance.check_self_collision():
+            game_over = True
+            print("Game Over")
 
 
 while running:
@@ -56,23 +113,15 @@ while running:
         handle_event()
 
     # Draw screen
-    screen.fill(BACKGROUND_COLOR)
-    pygame.draw.line(screen, WHITE, LINE_START, LINE_END, LINE_WIDTH)
+    draw_screen()
 
     # Draw snake
-    if not paused:
-        if snake.body[0].topleft == food.rect.topleft:
-            snake.grow_snake()
-            food.position = food.reset_position(snake.body)
-        snake.move()
-        snake.draw(screen)
-    else:
-        display_pause_screen()
+    display_game()
 
-    food.draw(screen)
-    #Update screen
+    check_for_game_over()
+
+    # Update screen
     pygame.display.flip()
-
-    clock.tick(10) # 10 FPS
+    clock.tick(10)  # 10 FPS
 
 pygame.quit()
--- snake.py
+++ snake.py
@@ -18,17 +18,8 @@ class Snake:
         new_x = head_x + self.velocity[0]
         new_y = head_y + self.velocity[1]
 
-        if new_y < LINE_START[1]:
-            new_y = SCREEN_HEIGHT - BODY_SIZE
-        elif new_y >= SCREEN_HEIGHT:
-            new_y = LINE_START[1]
-
-        if new_x < 0:
-            new_x = SCREEN_WIDTH - BODY_SIZE
-        elif new_x >= SCREEN_WIDTH:
-            new_x = 0
+        new_x, new_y = self.check_food_out_of_bound(new_x, new_y)
         new_head = pygame.Rect(new_x, new_y, BODY_SIZE, BODY_SIZE)
-
 
         self.body.insert(0, new_head)
         if not self.grow:
@@ -37,6 +28,23 @@ class Snake:
             self.grow = False
             self.colors = [HEAD_COLOR] + [BODY_COLOR] * (len(self.body) - 2) + [TAIL_COLOR]
         # print(self.body)
+
+    def check_self_collision(self):
+        head_pos = (self.body[0].topleft[0], self.body[0].topleft[1])
+        body_positions = {(segment.x, segment.y) for segment in self.body[1:]}
+
+        return head_pos in body_positions
+
+    def check_food_out_of_bound(self, new_x, new_y):
+        if new_y < LINE_START[1]:
+            new_y = SCREEN_HEIGHT - BODY_SIZE
+        elif new_y >= SCREEN_HEIGHT:
+            new_y = LINE_START[1]
+        if new_x < 0:
+            new_x = SCREEN_WIDTH - BODY_SIZE
+        elif new_x >= SCREEN_WIDTH:
+            new_x = 0
+        return new_x, new_y
 
     def grow_snake(self):
         self.grow = True
